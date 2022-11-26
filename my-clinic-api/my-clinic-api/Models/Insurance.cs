4d1696408ad4929232ac79181836b0b0420a0404
33185 <hameed_20180216@fci.helwan.edu.eg>
2022-11-26 13:22:22 +0200

    testPull

---
+++ my-clinic-api/my-clinic-api/Models/Insurance.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace my_clinic_api.Models
+{
+    public class Insurance
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string CompanyName { get; set; }
+
+        public int Discount { get; set; }
+    }
+}

5d3e487bd1175d65a5c605d5190aa80572f4c671
Amr1elmasry <56364993+Amr1elmasry@users.noreply.github.com>
2022-11-27 19:27:51 +0200

    TestNewGitIgnore

--- my-clinic-api/Controllers/WeatherForecastController.cs
+++ my-clinic-api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace my_clinic_api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Amr"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;

e399129e403d68a73c0b3f7716d93a89d85531fb
33185 <hameed_20180216@fci.helwan.edu.eg>
2022-11-27 11:40:35 +0200

    add area class

---
+++ my-clinic-api/my-clinic-api/Models/Area.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace my_clinic_api.Models
+{
+    public class Area
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public Cities cities { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name ="Area Name")]
+        public string AreaName { get; set; }
+
+
+    }
+}

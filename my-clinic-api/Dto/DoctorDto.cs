ea77b0d78db5a3817478a544b78ea9e00099c7b3
33185 <hameed_20180216@fci.helwan.edu.eg>
2022-12-03 15:29:07 +0200

    Add auto mapper

---
+++ my-clinic-api/Dto/DoctorDto.cs
@@ -0,0 +1,49 @@
+﻿using my_clinic_api.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace my_clinic_api.Dto
+{
+    public class DoctorDto 
+    {
+        public string? UserId { get; set; }
+
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+
+        public string? Passowrd { get; set; }
+
+        public string? FullName { get; set; }
+        public string? DoctorTitle { get; set; }
+
+        public float AvgRate { get; set; }
+
+        public string Bio { get; set; } = string.Empty;
+
+        public int NumberOfViews { get; set; }
+
+        public int WaitingTime { get; set; }
+
+        public bool IsConfirmedFromAdmin { get; set; }
+
+        public DaysOfBlock? DaysOfBlock { get; set; }
+
+        public DateTime? StartOfBlock { get; set; }
+        public DateTime? EndOfBlock { get; set; }
+
+        public Cities city { get; set; }
+        public string? Area { get; set; }
+        public string? Address { get; set; }
+
+        public Gender? Gender { get; set; }
+        public Department? Department { get; set; }
+
+        public ApplicationUser? User { get; set; }
+
+        public ICollection<Specialist>? Specialists { get; set; }
+        public ICollection<TimesOfWork>? TimesOfWorks { get; set; }
+        public ICollection<Insurance>? Insurances { get; set; }
+        public ICollection<Hospital>? Hospitals { get; set; }
+        public ICollection<RateAndReview>? RatesAndReviews { get; set; }
+    }
+}
---
+++ my-clinic-api/Profiles/DoctorProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace my_clinic_api.Profiles
+{
+    public class DoctorProfile : Profile
+    {
+        public DoctorProfile()
+        {
+            CreateMap<Models.Doctor, Dto.DoctorDto>();
+        }
+    }
+
+}

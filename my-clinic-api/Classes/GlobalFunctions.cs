42e2c5d353dc3cc58fc23456b083dbc0b30a5499
Amr Elmasry <amr1elmasry@outlook.com>
2023-02-06 19:55:15 +0200

    Edit Function GetNextWeekday

--- my-clinic-api/Classes/GlobalFunctions.cs
+++ my-clinic-api/Classes/GlobalFunctions.cs
@@ -4,10 +4,10 @@
     {
         public static DateTime GetNextWeekday(DayOfWeek day)
         {
-            DateTime result = DateTime.Now.Date;
-            while (result.DayOfWeek != day)
-                result = result.AddDays(1);
-            return result;
+            DateTime start = DateTime.Now.Date;
+            // The (... + 7) % 7 ensures we end up with a value in the range [0, 6]
+            int daysToAdd = ((int)day - (int)start.DayOfWeek + 7) % 7;
+            return start.AddDays(daysToAdd);
         }
     }
 }
